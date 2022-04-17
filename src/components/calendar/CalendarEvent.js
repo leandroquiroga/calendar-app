7b2e160e9c74d15451eba15cf572ca48ce2869ca
Leandro <leandroquiroga9514@gmail.com>
2022-04-16 22:12:10 -0300

    control: This components contols the event calendar

---
+++ src/components/calendar/CalendarEvent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const CalendarEvent = ({ event }) => {
+
+  const { title, user, notes } = event;
+  return (
+    <article className='d-flex flex-column justify-content-center'>
+      <span> {title} </span>
+      <strong>- { user.name } </strong>
+    </article>
+  )
+}
