1dc850fc557b00df1bd13ccc8b7eda4a6ccd07ca
Leandro <leandroquiroga9514@gmail.com>
2022-04-24 18:02:33 -0300

    add: Has been added a components provaider of the react-redux with our store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 import { AppRourter } from './router/AppRourter';
 
 import './index.css'
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { store } from './store/store';
 
 ReactDOM.render(
-  <AppRourter />,
+  <Provider store={store}>
+    <AppRourter />
+  </Provider>,
   document.getElementById('root')
 );
-
