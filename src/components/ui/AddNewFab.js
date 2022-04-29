4ef242b2fc8b2ca61a66430d1eacdcf53752ce3a
Leandro <leandroquiroga9514@gmail.com>
2022-04-28 22:05:28 -0300

    component: Has been added a new component <AddNewFab />

---
+++ src/components/ui/AddNewFab.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+import { uiOpenModal } from '../../actions/actions';
+
+export const AddNewFab = () => {
+  
+  const dispatch = useDispatch();
+
+  // Abre el modal 
+  const handleOpenModal = () => dispatch(uiOpenModal())
+  
+  return (
+    <button
+      className='btn btn-primary shadow rounded-circle button_icon'
+      onClick={handleOpenModal}
+    >
+      <i className="fa-solid fa-plus"></i>
+    </button>
+  )
+}
