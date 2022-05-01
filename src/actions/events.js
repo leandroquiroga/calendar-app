5c717df10a3a66e1b5d8c481b23eacba404f3d7b
leandroquiroga <leandroquiroga9514@gmail.com>
2022-05-01 17:42:54 -0300

    new: Has been added a news events - updateEvent and deleteEventNotes -

--- src/actions/events.js
+++ src/actions/events.js
@@ -12,3 +12,13 @@ export const eventSetActive = (event) => ({
   type: types.eventSetActive,
   payload: event
 })
+
+
+export const clearEventActive = () => ({ type: types.clearActiveEvent });
+
+export const updatedEvent = (event) => ({
+  type: types.eventUpdateNotes,
+  payload: event
+});
+
+export const deleteEventNotes = () => ({ type: types.eventDeleteNotes });
