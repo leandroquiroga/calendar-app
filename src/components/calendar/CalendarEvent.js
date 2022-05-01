708d276c404923e743f2ae534355944b3d31d481
leandroquiroga <leandroquiroga9514@gmail.com>
2022-05-01 17:36:04 -0300

    components: This component show the content of the event

--- src/components/calendar/CalendarEvent.js
+++ src/components/calendar/CalendarEvent.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 export const CalendarEvent = ({ event }) => {
 
-  const { title, user } = event;
+  const { title, user, notes } = event;
   return (
     <article className='d-flex flex-column justify-content-center'>
-      <span> {title} </span>
-      <strong>- { user.name } </strong>
+      <span className='my-1 fs-5'> {title} </span>
+      <strong className='my-1 fs-6'> -{user.name} </strong>
+      <small className='my-1 fs-6'>{ notes }</small>
     </article>
   )
 }
