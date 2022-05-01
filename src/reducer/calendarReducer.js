036d03f7e34074329485a4539190224ec0ac6dfc
leandroquiroga <leandroquiroga9514@gmail.com>
2022-05-01 17:45:11 -0300

    new: Has been added a two case in the condition 1- Update a event and other 2- Delete a event

--- src/reducer/calendarReducer.js
+++ src/reducer/calendarReducer.js
@@ -2,7 +2,8 @@ import moment from 'moment';
 import { types } from './../types/types';
 
 const initialState = {
-  events: [  {
+  events: [{
+    id: new Date().getTime(),
     title: 'Cita',
     notes: 'Llevar el mate',
     start: moment().toDate(),
@@ -17,11 +18,42 @@ const initialState = {
 };
 
 export const calendarReducer = (state = initialState, action) => {
+  
   switch (action.type) {
     case types.eventSetActive: 
       return {
         ...state,
         activeEvent: action.payload
+      }
+    case types.eventAddNew: 
+      return {
+        ...state,
+        events: [
+          ...state.events,
+          action.payload
+        ]
+      }
+    
+    case types.clearActiveEvent: 
+      return {
+        ...state,
+        activeEvent: null
+      }
+    case types.eventUpdateNotes:
+      return {
+        ...state,
+        events: state.events.map(
+          e => (e.id === action.payload.id) ? action.payload : e
+        )
+      };
+    
+    case types.eventDeleteNotes: 
+      return {
+        ...state, 
+        events: state.events.filter(
+          e => e.id !== state.activeEvent.id
+        ),
+        activeEvent: null
       }
     default:
       return state;
