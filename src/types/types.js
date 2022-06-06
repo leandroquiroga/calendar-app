7c4f81fb52ab99b3ea4664c85b39448b75be3204
leandroquiroga <leandroquiroga9514@gmail.com>
2022-06-06 17:36:09 -0300

    type: Configuration inital of the types

--- src/types/types.js
+++ src/types/types.js
@@ -1,13 +1,21 @@
 // Objeto que mantiene centralizado todos los tipos de la acciones
-
-
 export const types = {
   uiOpenModal: '[ui] Open modal',
   uiCloseModal: '[ui] Close modal',
 
+  eventStartAddNew: '[event] Start add new',
   eventAddNew: '[event] Add new',
   eventSetActive: '[event] Set active',
   eventUpdateNotes: '[event] Updated notes',
   clearActiveEvent: '[event] Clear notes',
-  eventDeleteNotes: '[event] Delete notes'
-}
+  eventDeleteNotes: '[event] Delete notes',
+  eventLoaded: '[event] Loaded notes',
+  eventLogout: '[event] Logout notes',
+
+  authChekingFinish: '[auth] Finish checking state',
+  authStartLogin: '[auth] Start login',
+  authLogin: '[auth] Login',
+  authStartRegister: '[auth] Start register',
+  authStartTokenRenew: '[auth] Start token renew',
+  authLogout: '[auth] Logout',
+};
