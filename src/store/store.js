155df76742d96eb50746c4e5b3fceb8681f8af02
leandroquiroga <leandroquiroga9514@gmail.com>
2022-06-06 17:35:19 -0300

    config: Configuration inital of the store

--- src/store/store.js
+++ src/store/store.js
@@ -9,18 +9,15 @@ import { composeWithDevTools } from '@redux-devtools/extension';
 import thunk from 'redux-thunk';
 import { uiReducer } from '../reducer/uiReducer';
 import { calendarReducer } from '../reducer/calendarReducer';
-
-
+import { authReducer } from '../reducer/authReducer';
 //const composeEnhancer = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
 const reducers = combineReducers({
   ui: uiReducer,
   calendar: calendarReducer,
+  auth: authReducer,
 })
 
 export const store = createStore(
   reducers,
   composeWithDevTools( applyMiddleware(thunk) ),
 );
-
-
