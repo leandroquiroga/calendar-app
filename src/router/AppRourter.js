1e897a5a37c77b1275012358e86fcdd45460eda1
leandroquiroga <leandroquiroga9514@gmail.com>
2022-06-06 17:37:35 -0300

    router: Has been created a component of routes of the application

--- src/router/AppRourter.js
+++ src/router/AppRourter.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux/es/exports';
 import { CalendarApp } from '../CalendarApp';
 import { Login } from '../components/auth/Login';
 import { Register } from '../components/auth/Register';
 import { PageNotFound } from '../components/pageNotFound/PageNotFound';
+import { startCheking } from '../actions/auth';
+import { Loading } from '../components/ui/Loading';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRourter = () => {
+
+  const dispatch = useDispatch();
+  const { checking } = useSelector(state => state.auth);
+
+  useEffect(() => {
+    dispatch(startCheking());
+  }, [dispatch]);
+
+  if (checking) return <Loading />;
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/login' element={ <Login />}/>
-        <Route path='/register' element={ <Register />} />
-        <Route path='/' element={ <CalendarApp />}/>
-        <Route path='*' element={ <PageNotFound/> } />
+        <Route path='/login' element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>
+        } />
+        <Route path='/register' element={
+          <PublicRoute>
+            <Register />
+          </PublicRoute>
+        } />
+        <Route path='/' element={
+          <PrivateRoute>
+            <CalendarApp />
+          </PrivateRoute>
+        } />
+        <Route path='*' element={
+          <PageNotFound />
+        } />
       </Routes>
     </BrowserRouter>
   )
-}
+};
