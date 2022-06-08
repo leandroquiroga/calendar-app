95c54dec842a1e305bebb3b68cfee3cff7d015a4
leandroquiroga <leandroquiroga9514@gmail.com>
2022-06-08 18:16:59 -0300

    component: This component make the dispatch of startUserLogout and return the name of the state of application

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,15 +1,26 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { startUserLogut } from '../../actions/auth';
 
 export const Navbar = () => {
+  
+  const { auth } = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  const handleSingOut = () => dispatch(startUserLogut());
+  
   return (
-    <nav className='navbar navbar-dark bg-dark'>
+    <nav className='navbar navbar-primary bg-primary'>
       <section className='container-fluid'>
-        <span className='text-white'>Hola, Leandro !</span>
-        <button className='btn btn-danger p-2 rounded'>
+        <span className='text-white text-capitalize'>Hola, {auth.name} !</span>
+        <button
+          className='btn btn-outline-light p-2 rounded'
+          onClick={handleSingOut}
+        >
           <i className="fa-solid fa-arrow-right-from-bracket"></i>
           <span> Logout </span>
         </button>
       </section>
     </nav>
   )
-}
+};
