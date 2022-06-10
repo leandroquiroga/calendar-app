85b90de50b7a0176a3076553b84e8311ff793e24
leandroquiroga <leandroquiroga9514@gmail.com>
2022-06-09 21:53:31 -0300

    new: Has been created a new event of the reducer - deleteEventNotes

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,24 +1,101 @@
-
 import { types } from './../types/types';
+import { fetchWithToken } from '../helpers/fetchAPI';
+import { prepareEvents } from '../helpers/prepareEvents';
 
-
-export const eventAddNew = (event) => ({
+const eventAddNew = (event) => ({
   type: types.eventAddNew, 
   payload: event,
 });
 
+const eventLoaded = (event) => ({
+  type: types.eventLoaded,
+  payload: event
+});
+
+const updatedEvent = (event) => ({
+  type: types.eventUpdateNotes,
+  payload: event
+});
+
+const deleteEventNotes = () => ({ type: types.eventDeleteNotes });
+
+export const eventStartAddNew = (event) => {
+  return async (dispatch, getState) => {
+
+    const { uid, name } = getState().auth;
+    
+    try {
+      const response = await fetchWithToken('events', event, 'POST');
+      const body = await response.json();
+
+      if (body.ok) {
+        event.id = body.event.id
+        event.user = {
+          uid,
+          name,
+        };
+        dispatch(eventAddNew(event));
+      }
+      
+    } catch (error) {
+      console.log(error)
+    }
+  };
+};
 
 export const eventSetActive = (event) => ({
   type: types.eventSetActive,
   payload: event
 })
 
-
 export const clearEventActive = () => ({ type: types.clearActiveEvent });
 
-export const updatedEvent = (event) => ({
-  type: types.eventUpdateNotes,
-  payload: event
-});
+export const eventStartLoading = () => {
+  return async(dispatch) => {
+    try {
+      const response = await fetchWithToken('events');
+      const body = await response.json(); 
+      const events = prepareEvents(body.event);
+      
+      dispatch(eventLoaded(events))
+    } catch (error) {
+      console.log(error)
+    }
+  };
+};
 
-export const deleteEventNotes = () => ({ type: types.eventDeleteNotes });
+export const eventStartUpdate = (event) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetchWithToken(`events/${event.id}`, event, 'PUT');
+      const body = await response.json();
+
+      if (body.ok)
+        return dispatch(updatedEvent(event));
+
+      console.log(body.msg);
+    } catch (error) {
+      console.log(error)
+    }
+  }
+};
+
+export const eventStartDelete = () => {
+  return async (dispatch, getState) => {
+
+    const { id } = getState().calendar.activeEvent;
+    
+    try {
+      const response = await fetchWithToken(`events/${id}`, {}, 'DELETE');
+      const body = await response.json();
+      
+      if (body.ok)
+        return dispatch(deleteEventNotes());
+      
+      console.log(body.msg)
+      
+    } catch (error) {
+      console.log(error)
+    }
+  }
+};
