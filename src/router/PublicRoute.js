b70f3493dc68f88b790e20c98e3ff1b5f8447fd7
leandroquiroga <leandroquiroga9514@gmail.com>
2022-06-09 21:50:46 -0300

    config: Configuration of the routes public of the application

--- src/router/PublicRoute.js
+++ src/router/PublicRoute.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useSelector } from 'react-redux';
 
 export const PublicRoute = ({ children }) => {
   const { uid } = useSelector(state => state.auth);
