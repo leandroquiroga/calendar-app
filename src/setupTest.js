4cb92be2444013fd1ad899d4e2df148583096ad7
leandroquiroga <leandroquiroga9514@gmail.com>
2022-06-13 19:26:06 -0300

    config: Has been modified the configuration by HTML Canvas

--- src/setupTest.js
+++ src/setupTest.js
@@ -4,4 +4,6 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import { createSerializer } from 'enzyme-to-json';
 
 Enzyme.configure({ adapter: new Adapter() });
-expect.addSnapshotSerializer(createSerializer({mode: 'deep'}));
+expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
+
+HTMLCanvasElement.prototype.getContext = () => {};
