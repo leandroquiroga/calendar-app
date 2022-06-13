e0f4189a67abbfeb3c1488067ad3ebc38e3faeb7
leandroquiroga <leandroquiroga9514@gmail.com>
2022-06-13 19:22:30 -0300

    new: This component allow delete a new event

--- src/components/ui/DeleteEventFab.js
+++ src/components/ui/DeleteEventFab.js
@@ -1,22 +1,20 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { deleteEventNotes } from '../../actions/events';
-
+import { useDispatch } from 'react-redux';
+import { eventStartDelete } from '../../actions/events';
 
 export const DeleteEventFab = () => {
 
   const dispatch = useDispatch();
-  const {activeEvent} = useSelector(state => state.calendar)
 
   // Elimina el evento
-  const handleDeleteNotes = () => dispatch(deleteEventNotes(activeEvent.id))
+  const handleDeleteNotes = () => dispatch(eventStartDelete())
 
   return (
     <button
       className='btn btn-danger rounded-circle shadow button_icon__delete'
       onClick={handleDeleteNotes}
     >
-        <i className="fa-solid fa-trash-can"></i>
+      <i className="fa-solid fa-trash-can"></i>
     </button>
   )
-}
+};
